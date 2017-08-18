9af47e3e962a346ea9c68956c9f70415f436b195
Zac Connelly <zac@mimi.io>
2017-08-18 09:33:53 +0200

    update selenium selector and README

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ virtualenv -p $(which python3.6) venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+
+### Actually Running It
+
+```bash
+# start a selenium server
+java -jar -Dwebdriver.gecko.driver=./geckodriver_mac selenium-server-standalone-3.0.1.jar
+```
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from getpass import getpass
 from subprocess import call
+import os
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# Login and Get to the Project Page
+# Constants
 password = getpass("Password: ")
+email = "zconnelly13@gmail.com"
+memrise_url = "https://www.memrise.com/course/1588576/random/edit/database/2577054/?page=%s"  # noqa
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Login and Get to the Project Page
 driver = webdriver.Chrome()
 driver.get("https://www.memrise.com/course/1559979/harry-potter-und-das-verwunschene-kind/edit/#l_5884845")  # noqa
 elem = driver.find_element_by_css_selector('a[href="/login/"]')
 elem.click()
-elem = driver.find_element_by_css_selector('a[href="/join/google-oauth2/?next=/home/"]')  # noqa
+elem = driver.find_element_by_css_selector('a[href="/join/google-oauth2/"]')  # noqa
 elem.click()
 elem = driver.find_element_by_css_selector('input[type="email"]')
-elem.send_keys("zconnelly13@gmail.com")
+elem.send_keys(email)
 elem.send_keys(Keys.RETURN)
 time.sleep(5)
 elem = driver.find_element_by_css_selector('input[type="password"]')
@@ -24,7 +30,7 @@ time.sleep(5)
 
 
 for page in range(1, 4):
-    driver.get("https://www.memrise.com/course/1588576/random/edit/database/2577054/?page=%s" % str(page))  # noqa
+    driver.get(memrise_url % str(page))  # noqa
     # driver.get("https://www.memrise.com/course/1588512/der-songtext/edit/database/2576989/?page=%s" % str(page))  # noqa
     # driver.get("https://www.memrise.com/course/1588458/kinderbucher/edit/database/2576930/?page=%s" % str(page))  # noqa
     # driver.get("https://www.memrise.com/course/1584764/erwachsen-werd-ich-vielleicht-spater/edit/database/2573062/?page=%s" % str(page))  # noqa
@@ -38,7 +44,7 @@ for page in range(1, 4):
         call(['say -v "Anna" "%s" -o audio/%s.mp4' % (word, str(i))],
              shell=True)
         file_input = thing.find_element_by_css_selector('input[type="file"]')
-        file_path = '/Users/zacmimi/personal_projects/memrise_audio_generator/audio/%s.mp4' % str(i)  # noqa
+        file_path = '%s/audio/%s.mp4' % (dir_path, str(i))  # noqa
         file_input.send_keys(file_path)
         i = i + 1
     time.sleep(5)
